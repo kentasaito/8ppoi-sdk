51b03242e193553aef58f2709167cde4e8466cd1
Kenta Saito <kenta.saito@sagittar.org>
2025-04-04 12:29:35 +0900

    2025-04-04 12:29:35

--- main.ts
+++ main.ts
@@ -2,35 +2,17 @@ import { Hono } from "@hono/hono";
 import { serveStatic } from "@hono/hono/deno";
 import { streamExec } from "@kenta/stream-exec";
 
-interface ContextVariables {
-  memberId: string;
-}
+let memberId = "unpublished_member";
+await streamExec(
+  "gh",
+  { args: ["api", "user", "--jq", ".id"] },
+  "",
+  (stdoutMessage) => memberId = stdoutMessage,
+  () => {},
+);
 
-const app = new Hono<{ Variables: ContextVariables }>();
+const app = new Hono();
 
-// メンバーID取得
-app.use("*", async (c, next) => {
-  c.set(
-    "memberId",
-    await (async () => {
-      let id = "unpublished_member";
-      await streamExec(
-        "gh",
-        { args: ["api", "user", "--jq", ".id"] },
-        (stdoutMessage) => id = stdoutMessage,
-        () => {},
-      );
-      return id;
-    })(),
-  );
-  await next();
-});
-
-app.get("/*", (c, next) => {
-  const memberId = c.get("memberId");
-  return serveStatic({
-    root: `./cartridges/${memberId}/`,
-  })(c, next);
-});
+app.get("/*", serveStatic({ root: `./cartridges/${memberId}/` }));
 
 Deno.serve(app.fetch);
