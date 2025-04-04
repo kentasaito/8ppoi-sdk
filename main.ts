6f54b283d074075bdde9ef77a897fb46e2056c69
Kenta Saito <kenta.saito@sagittar.org>
2025-04-04 12:32:39 +0900

    2025-04-04 12:32:39

--- main.ts
+++ main.ts
@@ -2,14 +2,20 @@ import { Hono } from "@hono/hono";
 import { serveStatic } from "@hono/hono/deno";
 import { streamExec } from "@kenta/stream-exec";
 
-let memberId = "unpublished_member";
-await streamExec(
+let memberId = "";
+const code = await streamExec(
   "gh",
   { args: ["api", "user", "--jq", ".id"] },
   "",
   (stdoutMessage) => memberId = stdoutMessage,
   () => {},
 );
+if (code !== 0) {
+  console.error("Failed to get member ID");
+  console.error("Please make sure you are logged in to GitHub CLI");
+  console.error("You can log in using the command: gh auth login");
+  Deno.exit(1);
+}
 
 const app = new Hono();
 
